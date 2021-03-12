36373fb438796bacd084763b1717e879945c9383
jobinau <jobinau@gmail.com>
2021-03-12 19:08:14 +0530

    parent of the blocking session

--- analysis_queries.sql
+++ analysis_queries.sql
@@ -3,4 +3,7 @@ SELECT wait_event,count(*) FROM pg_pid_wait
 WHERE pid in (select pid from pg_get_activity where state='idle in transaction')
 GROUP BY wait_event ORDER BY 2;
 
-
+---Which session is at the top of the blocking
+select blocking_pid,statement_in_blocking_process,count(*)
+ from pg_get_block where blocking_pid not in (select blocked_pid from pg_get_block)
+ group by 1,2;
