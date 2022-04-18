dcce02bb80f647c34defebe07db78be43bb09641
Jobin Augustine <jobinau@gmail.com>
2022-04-18 08:43:38 +0530

    Redesign of imphistory.sh to accomodate accidental full gather outputs

--- imphistory.sh
+++ imphistory.sh
@@ -6,30 +6,32 @@ if [ $# -eq 0 ]
 fi
 for f in "$@"
 do 
-    ##Check whether the files are really the partial gather outputs
-    partial=`zcat $f | head -n 12 | grep template1 | wc -l` ;
-    ##Data collection timestamp. This info can be inserted for collect_ts of each line
-    coll_ts=`zcat $f | head -n 15 | sed -n '/COPY pg_gather/ {n; s/\([0-9-]*\s[0-9:\.+]*\).*/\1/; p}'`
-    echo
-    echo "\Importing :"$coll_ts;
-    if [ $partial = 1 ]; then
-      #import gather data and copy it to history tables
-      echo "Partial"
-        #In a real customer enviorment testing, an additional column appeared for pg_pid_wait like : ?column?|8459	ClientRead
-        #This don't have a good explanation yet and treated as unknown bug. 
-        #Added 2 lines to mitigate this problem: /^[[:space:]]*$/d      and    s/^\?column?|\(.*\)/\1/
-        #TODO : Observe over a period of time and remove those 2 lines if possible.
-        zcat $f | sed -n '
-        /^COPY/, /^\\\./ {
-          s/COPY pg_get_activity (/COPY pg_get_activity (collect_ts,/
-          s/COPY pg_pid_wait (/COPY pg_pid_wait (collect_ts,/
-          s/COPY pg_get_db (/COPY pg_get_db (collect_ts,/
-          /^[[:space:]]*$/d
-          s/^\?column?|\(.*\)/\1/
-          /^\(COPY\|\\\.\)/! s/^/'"$coll_ts"\\t'/ # All lines other than those starting with COPY or \. should have coll_ts inserted
-          p
-        }' | psql "options='-c search_path=history -c synchronous_commit=off'"  -f - 
-    else
-      echo "Full"
-    fi
+  ##Data collection timestamp. This info can be inserted for collect_ts of each line
+  coll_ts=`zcat $f | head -n 15 | sed -n '/COPY pg_gather/ {n; s/\([0-9-]*\s[0-9:\.+]*\).*/\1/; p}'`
+  printf "\nImporting %s \n" "$coll_ts"
+  #In a real customer enviorment testing, an additional column appeared for pg_pid_wait like : ?column?|8459	ClientRead
+  #This don't have a good explanation yet and treated as unknown bug. 
+  #Added 2 lines to mitigate this problem: /^[[:space:]]*$/d      and    s/^\?column?|\(.*\)/\1/
+  #TODO : Observe over a period of time and remove those 2 lines if possible.
+  zcat $f | sed -n '
+  /^COPY/, /^\\\./ {
+    s/COPY pg_get_activity (/COPY pg_get_activity (collect_ts,/
+    s/COPY pg_pid_wait (/COPY pg_pid_wait (collect_ts,/
+    s/COPY pg_get_db (/COPY pg_get_db (collect_ts,/
+    s/COPY pg_replication_stat(/COPY pg_replication_stat (collect_ts,/
+    /^COPY pg_srvr/, /^\\\./d  #Delete any full gather information
+    /^COPY pg_get_roles/, /^\\\./d   # -do-
+    /^COPY pg_get_confs/, /^\\\./d   # -do-
+    /^COPY pg_get_file_confs/, /^\\\./d  #-do-
+    /^COPY pg_get_class/, /^\\\./d  #-do-
+    /^COPY pg_get_index/, /^\\\./d  #-do-
+    /^COPY pg_get_rel/, /^\\\./d  #-do-
+    /^COPY pg_tab_bloat/, /^\\\./d  #-do-
+    /^COPY pg_get_toast/, /^\\\./d  #-do-
+    /^COPY pg_get_extension/, /^\\\./d  #-do-
+    /^[[:space:]]*$/d
+    s/^\?column?|\(.*\)/\1/
+    /^\(COPY\|\\\.\)/! s/^/'"$coll_ts"\\t'/ # All lines other than those starting with COPY or \. should have coll_ts inserted
+    p
+  }' | psql "options='-c search_path=history -c synchronous_commit=off'"  -f - 
 done
