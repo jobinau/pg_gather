e28274f6fd1009d0c3bd49f51bdb02103bdacdb6
Jobin Augustine <jobinau@gmail.com>
2025-03-16 17:50:18 +0000

    Data collection change for PG18

--- gather.sql
+++ gather.sql
@@ -269,8 +269,8 @@ select archived_count,last_archived_wal,last_archived_time,last_failed_wal,last_
 
 --WAL stats
 \if :pg14
-\echo COPY pg_get_wal(wal_records,wal_fpi,wal_bytes,wal_buffers_full,wal_write,wal_sync,wal_write_time,wal_sync_time,stats_reset) FROM stdin;
-COPY (SELECT wal_records,wal_fpi,wal_bytes,wal_buffers_full,wal_write,wal_sync,wal_write_time,wal_sync_time,stats_reset FROM pg_stat_wal) TO stdout;
+\echo COPY pg_get_wal(wal_records,wal_fpi,wal_bytes,wal_buffers_full,stats_reset) FROM stdin;
+COPY (SELECT wal_records,wal_fpi,wal_bytes,wal_buffers_full,stats_reset FROM pg_stat_wal) TO stdout;
 \echo '\\.'
 \endif
 
