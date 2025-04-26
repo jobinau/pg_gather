fdd188fcecb9c00d64244da7b06f8f656ba8ca10
Jobin Augustine <jobinau@gmail.com>
2025-04-26 16:43:37 +0530

    Move the popup to first element of the table. This will allow future poppus for other columns and possibilty of mouseover features on the popup

--- dev/report.sql
+++ dev/report.sql
@@ -14,11 +14,11 @@
 \echo .high { border: 5px solid red;font-weight:bold}
 \echo .lime { font-weight:bold;background-color: #FFD}
 \echo .lineblk {float: left; margin:2em }
-\echo .thidden tr { td:nth-child(2),th:nth-child(2) {display: none} td:first-child {color:blue}}
+\echo .thidden tr { td:nth-child(2),th:nth-child(2) {display: none} td:first-child {color:blue;position: relative;}}
 \echo #bottommenu { position: fixed; right: 0px; bottom: 0px; padding: 5px; border : 2px solid #AFAFFF; border-radius: 5px; z-index: 2}
 \echo #cur { font: 5em arial; position: absolute; color:brown; animation: vanish 2s ease forwards; }  /*sort indicator*/
 \echo #dtls,#finditem,#paramtune,#menu { font-weight:initial;line-height:1.5em;position:absolute;background-color:#FAFFEA;border: 2px solid blue; border-radius: 5px; padding: 1em;box-shadow: 0px 20px 30px -10px grey; z-index: 1}
-\echo #dtls { margin-left: -10px }
+\echo #dtls { margin-left: -0.2em; left:100%; top: 50%; width: max-content; color: black;}
 \echo @keyframes vanish { from { opacity: 1;} to {opacity: 0;} }
 \echo summary {  padding: 1rem; font: bold 1.2em arial;  cursor: pointer } 
 \echo footer { text-align: center; padding: 3px; background-color:#d2f2ff}
@@ -1315,14 +1315,14 @@ LEFT JOIN pg_tab_bloat b ON c.reloid = b.table_oid) AS tabs,
 \echo   if(tab.id=="tblsess") el.innerHTML=sessdtls(tr);
 \echo   if(tab.id=="tblusr") el.innerHTML=userdtls(tr);
 \echo   if(tab.id=="tblcs") el.innerHTML=dbcons(tr);
-\echo   tr.cells[2].appendChild(el);
+\echo   tr.cells[0].appendChild(el);
 \echo })));
 \echo document.querySelectorAll(".thidden tr td:first-child").forEach(td => td.addEventListener("dblclick", (() => {
-\echo   navigator.clipboard.writeText(td.parentNode.cells[2].children[0].innerText);
+\echo   navigator.clipboard.writeText(td.parentNode.cells[0].children[0].innerText);
 \echo   flash("Details copied to clipboard");
 \echo })));
 \echo document.querySelectorAll(".thidden tr td:first-child").forEach(td => td.addEventListener("mouseout", (() => {
-\echo    td.parentNode.cells[2].children[0].remove();
+\echo    td.parentNode.cells[0].children[0].remove();
 \echo })));
 \echo let elem=document.getElementById("bottommenu")
 \echo elem.onmouseover = function() { document.getElementById("menu").style.display = "block"; }
