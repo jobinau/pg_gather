4a09333ed32273fbcf5bae0a8f4b3967bb116515
Jobin Augustine <jobinau@gmail.com>
2026-02-27 15:20:29 +0530

    Fix for crosstab report

--- docs/crosstab.sql
+++ docs/crosstab.sql
@@ -3,11 +3,14 @@
 --   tablefunc is part of PostgreSQL contrib modules              --
 --------------------------------------------------------------------
 --Findout the wait events and prepare the columns for the crosstab report
-SELECT STRING_AGG(col,',') AS cols FROM 
-(SELECT COALESCE(wait_event,'CPU') || ' int' "col"
-FROM history.pg_pid_wait WHERE wait_event IS NULL OR 
-  wait_event NOT IN ('ArchiverMain','AutoVacuumMain','BgWriterHibernate','BgWriterMain','CheckpointerMain','LogicalApplyMain','LogicalLauncherMain','RecoveryWalStream','SysLoggerMain','WalReceiverMain','WalSenderMain','WalWriterMain','CheckpointWriteDelay','PgSleep','VacuumDelay')  
-GROUP BY wait_event ORDER BY 1) as A \gset
+SELECT STRING_AGG(col, ',') AS cols
+FROM (
+  SELECT DISTINCT '"' || COALESCE(wait_event,'CPU') || '" int' AS col
+  FROM history.pg_pid_wait
+  WHERE wait_event IS NULL OR wait_event NOT IN ('ArchiverMain','AutoVacuumMain','BgWriterHibernate','BgWriterMain','CheckpointerMain','LogicalApplyMain','LogicalLauncherMain','RecoveryWalStream','SysLoggerMain','WalReceiverMain','WalSenderMain','WalWriterMain','CheckpointWriteDelay','PgSleep','VacuumDelay')
+  ORDER BY 1
+) AS A
+\gset
 --Run a crosstab query 
 SELECT *
 FROM crosstab(
